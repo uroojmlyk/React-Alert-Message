e5403e33514a972cc67a9518056e5800192f835b
Urooj Rasheed <uroojr335@gmail.com>
2026-01-04 19:44:50 +0500

    Learn Concept of Alert Message

---
+++ App.js
@@ -0,0 +1,15 @@
+import MyButton from './components/MyButton';
+import Title from './components/Title';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <Card />
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
